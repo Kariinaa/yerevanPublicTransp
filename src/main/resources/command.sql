3d6c749f0f5182026fee188958772a275f536579
Karina Gyamjyan <karinagyamjyan@Karinas-MacBook-Air.local>
2017-11-25 17:45:57 +0400

    sql commands added

--- src/main/resources/command.sql
+++ src/main/resources/command.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS public_transportation_production;
 CREATE DATABASE IF NOT EXISTS public_transportation_test;
 
 USE public_transportation_production;
+USE public_transportation_test;
 
 CREATE TABLE IF NOT EXISTS user (
   id        INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -9,3 +10,49 @@ CREATE TABLE IF NOT EXISTS user (
   pass_hash VARCHAR(50)
 )
   ENGINE INNODB;
+
+CREATE TABLE IF NOT EXISTS stop (
+  id        INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  longitute DOUBLE,
+  latitude  DOUBLE
+)
+  ENGINE INNODB;
+
+
+CREATE TABLE IF NOT EXISTS favorite (
+  user_id INT,
+  stop_id INT,
+  FOREIGN KEY (user_id) REFERENCES user (id),
+  FOREIGN KEY (stop_id) REFERENCES stop (id)
+
+)
+  ENGINE INNODB;
+
+CREATE TABLE IF NOT EXISTS vehicle_type (
+  id   INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(10)
+)
+  ENGINE INNODB;
+
+CREATE TABLE IF NOT EXISTS vehicle (
+  id              INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  number          INT NOT NULL,
+  vehicle_type_id INT NOT NULL,
+  cycle_length    INT,
+  quantity        INT,
+  `interval`      INT,
+  FOREIGN KEY (vehicle_type_id) REFERENCES vehicle (id)
+
+)
+  ENGINE INNODB;
+
+CREATE TABLE IF NOT EXISTS stop_vehicle (
+  stop_id    INT,
+  vehicle_id INT,
+  FOREIGN KEY (stop_id) REFERENCES stop (id),
+  FOREIGN KEY (vehicle_id) REFERENCES vehicle (id)
+)
+  ENGINE INNODB;
+
+
+
